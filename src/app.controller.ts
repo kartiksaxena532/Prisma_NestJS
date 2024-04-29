d6026e335a476b0a5b3ce5f69a91845aa9ebd40f
Kartik Saxena <karrtiksaxena@gmail.com>
2024-04-29 17:03:53 +0530

    added prisma cleint

---
+++ src/app.controller.ts
@@ -0,0 +1,36 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+} from '@nestjs/common';
+import { ProductService } from './app.service';
+import { Product } from '@prisma/client';
+
+@Controller('products')
+export class ProductController {
+  constructor(private readonly productService: ProductService) {}
+
+  @Post()
+  create(@Body() productData: Product): Promise<Product> {
+    return this.productService.create(productData);
+  }
+
+  @Get()
+  findAll(): Promise<Product[]> {
+    return this.productService.findAll();
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() productData: Product): Promise<Product> {
+    return this.productService.update(parseInt(id), productData);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string): Promise<void> {
+    return this.productService.delete(parseInt(id));
+  }
+}
