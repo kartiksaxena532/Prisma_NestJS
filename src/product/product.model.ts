7e7570ee572234d3c469926d87b5320dc2e6a58d
Kartik Saxena <karrtiksaxena@gmail.com>
2024-05-03 16:08:13 +0530

    resolved typo

--- src/product/product.model.ts
+++ src/product/product.model.ts
@@ -1,7 +1,11 @@
-import { Prisma, ProductCreateInput } from "@prisma/client";
+import { Prisma } from "@prisma/client";
 
-export class Product implements ProductCreateInput {
-     id: number;
-     title: string;
-     description?: string;
+
+export class Product implements Prisma.ProductCreateInput {
+     id: string;    
+     name:string;
+     price:number;
+     category:string;
+     description?:string; //as this can be optional
+     stockQuantity:number;
 }
